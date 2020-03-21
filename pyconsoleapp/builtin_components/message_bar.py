b6cea8ad845414ded37b0d638bee1fca2203e922
JamesAIzzard <jamesaizzard@gmail.com>
2020-03-21 09:54:46 +0000

    Bug fix.

--- pyconsoleapp/builtin_components/message_bar.py
+++ pyconsoleapp/builtin_components/message_bar.py
@@ -6,7 +6,7 @@ class MessageBar(ConsoleAppComponent):
         output = ''
         if self.app.error_message:
             output = output+'/!\\ Error: {}\n'.format(self.app.error_message)
-            output = output+('-'*self.app.terminal_width_chars)+'\n'
+            output = output+('-'*self.app.configs.terminal_width_chars)+'\n'
             self.app.error_message = None
         if self.app.info_message:
             output = output+'[i] Info: {}\n'.format(self.app.info_message)
