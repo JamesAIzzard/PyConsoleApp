a028ebcfdb49094652c297604ea3ed79a8d2d109
James Izzard <jamesaizzard@gmail.com>
2020-03-21 04:07:02 +0000

    More work on route data.

--- pyconsoleapp/route_data.py
+++ pyconsoleapp/route_data.py
@@ -4,25 +4,13 @@ if TYPE_CHECKING:
     from pyconsoleapp.console_app import ConsoleApp
     from pyconsoleapp.utility_service import UtilityService
 
-class RouteData():
-
-    def __init__(self):
-        self._data:Dict[str, Any] = {}
-
-    def __setattr__(self, name: str, value: Any) -> None:
-        self._data[name] = value
-
-    def __getattribute__(self, name: str) -> Any:
-        return self._data[name]
-
-
 class RoutedDatabase():
 
     def __init__(self, app):
         self._selected_route:List[str] = app.route
         self._app: 'ConsoleApp' = app
         self._utility_service:'UtilityService' = utility_service        
-        self._data: Dict[str, RouteData] = {}
+        self._data: Dict[str, Dict[str, Any]] = {}
 
     def __call__(self, route: List[str]) -> 'RoutedDatabase':
         route = self._app.complete_relative_route(route)
@@ -30,7 +18,14 @@ class RoutedDatabase():
         return self
 
     def __setattr__(self, name: str, value: Any) -> None:
-        pass
+        route_key = self._utility_service.stringify_route(self._selected_route)
+        # If the property is in scope;
+        if self._in_scope(name):
+            # Set it;
+            self.__getattribute__(name) = value
+        # Otherwise, create it;
+        if route_key in self._data.keys():
+            
 
     def __getattribute__(self, name:str) -> Any:
         pass
